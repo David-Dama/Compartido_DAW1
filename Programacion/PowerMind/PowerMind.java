fef5fdd474769e4e0d169acd3745064e9cbf02c3
KamiZhiEND <xuweidavid27@gmail.com>
2025-12-12 14:18:40 +0100

    Optimizacion

--- Programacion/PowerMind/PowerMind.java
+++ Programacion/PowerMind/PowerMind.java
@@ -6,30 +6,39 @@ import java.util.Scanner;
 public class PowerMind {
 
     public static void main(String[] args){
-        int opcion, numPartida = 1;
+        /* Array para guardar las estadisticas:
+        stats[0]=puntos obtenidos
+        stats[1]=intentos
+        stats[2]=partidas ganadas
+        stats[3]=partidas perdidas
+        stats[4]=partidas abandonadas
+        */
         int[] stats = new int[5];
+        // Declaracion inicial de parametros necesarios
+        int opcion, numPartida = 1;
+        // Creamos un scanner
         Scanner sc = new Scanner(System.in);
 
-        // Bucle principal
+        // Bucle principal del programa
         do {
-            System.out.println("----------------------------");
-            System.out.printf("PARTIDA NUMERO %d\n\n", numPartida);
             opcion = eleccionPrincipal(sc);
             if (opcion==1){
-                iniciarPartida(generarNum(), stats, sc);
+                iniciarPartida(generarNum(4), stats, sc, numPartida);
                 numPartida++;
             }
             else {
                 mostrarEstadistica(stats);
             }
         }while(opcion != 3);
+
+        // Cierre del scanner una vez finalizado el programa
         sc.close();
     }
 
     // Metodo para pedir numero entero con validacion ya que vamos a pedir muchos enteros
     public static int pedirEntero(Scanner sc) {
         while (!sc.hasNextInt()){
-            System.out.println("Intoduzca un número");
+            System.err.print("Introduzca solo números enteros: ");
             // Descartamos el input invalido
             sc.next();
         }
@@ -49,36 +58,22 @@ public class PowerMind {
         System.out.println();
         // Bucle de validacion del rango de enteros
         while(opcion <1 || opcion >3){
-            System.out.println("Introduce sólo un numero entre el 1-3!");
+            System.err.print("Introduce sólo un numero entre el 1-3: ");
             opcion=pedirEntero(sc);
         }
         return opcion;
     }
 
-    // Metodo para generar el numero correcto
-    public static int[] generarNum (){
-        int [] array = new int[4];
-        // Array de booleanos para guardar del 0-9, que se auto rellena con false
-        boolean [] usado = new boolean[10];
-        // Bucle rellena array
-        for (int i = 0; i < array.length; i++){
-            int num;
-            do {
-                // Genera randoms del 0 al 9
-                num = (int) ((Math.random() * 10));
-            } while (usado[num]);
-            array[i] = num;
-            // Cambiamos a true la posicion que tiene a num como indice
-            // Así si vuelve a salir, el bucle se ejecutara
-            usado[num] = true;
-        }
-        return array;
-    }
-
     // Metodo para opcion 1. Iniciar partida
-    public static void iniciarPartida(int[] correctNum, int[] resultado, Scanner sc){
-        int [] secretNum = new int[4];
+    public static void iniciarPartida(int[] numCorrecto, int[] stats, Scanner sc, int numPartida){
+        // Array que el usuario va a rellenar
+        int [] numIntroducido = new int[4];
+        // Array de pista que indicará si los numeros están en la posición correcta o no
+        char [] sne = new char[4];
         int numIntentos = 1;
+
+        System.out.println("----------------------------");
+        System.out.printf("PARTIDA NUMERO %d\n\n", numPartida);
 
         while (numIntentos <= 5) {
             System.out.println("----------------------------");
@@ -86,56 +81,51 @@ public class PowerMind {
             System.out.println("\nRECUERDA: Para salir debe escribir -1.");
 
             // Bucle para rellenar array
-            for (int i = 0; i < secretNum.length; i++) {
+            for (int i = 0; i < numIntroducido.length; i++) {
                 do {
-                    System.out.printf("Introduce el número secreto de la posición %d: ", i);
-                    secretNum[i] = pedirEntero(sc);
+                    System.out.printf("Introduce el número de la posición %d: ", i);
+                    numIntroducido[i] = pedirEntero(sc);
                     // Condicion para sacarnos del metodo
-                    if (secretNum[i] == -1) {
+                    if (numIntroducido[i] == -1) {
                         // -1 indica partida abandonada, nos saca del metodo directamente
-                        resultado[4] += 1;
-                        // -1 intentos porque ha abandonado el intento actual, entonces este intento no cuenta
-                        resultado[2] += numIntentos-1;
+                        stats[4] += 1;
+                        // Intentos -1 porque ha abandonado el intento actual, entonces este intento no cuenta
+                        stats[1] += numIntentos-1;
                         // salimos del metodo
                         return;
                     }
-                    if (secretNum[i] < 0 || secretNum[i] > 9) {
+                    // Condicion para enviar el mensage de error
+                    if (numIntroducido[i] < 0 || numIntroducido[i] > 9) {
                         System.out.println("Número inválido. Solo se acepta número entre 0 y 9.");
                     }
-                } while (secretNum[i] < 0 || secretNum[i] > 9);
+                } while (numIntroducido[i] < 0 || numIntroducido[i] > 9);
             }
 
-            char [] SNO = comprobar(secretNum,correctNum);
+            // Llamada al metodo comprobar para que rellene el array de pistas
+            sne = comprobar(numIntroducido,numCorrecto);
 
-            //Salida de datos
-            System.out.println("""
-                       Recuerda:
-                       S == En la posición correcta
-                       N == No está
-                       O == En posición incorrecta
-                    """);
+            mostrarEstado(numIntroducido, sne);
 
-            System.out.println(Arrays.toString(secretNum));
-            System.out.println(" ↑  ↑  ↑  ↑");
-            System.out.println(Arrays.toString(SNO));
-
-            //Si encuentra el número, salimos del bucle y cambiamos el valor de la bandera para que en la siguiente
-            //Condicion retorne el valor correspondiente
-            if (Arrays.equals (secretNum, correctNum)){
+            // Condicion de que el jugador ha adivinado(ganado)
+            if (Arrays.equals (numIntroducido, numCorrecto)){
+                // Cambiamos las estadisticas dependiendo del número de intentos
                 switch (numIntentos){
                     case 1, 2:
-                        resultado[0] += 3;
-                        resultado[1] += 1;
+                        // Suma 3 puntos
+                        stats[0] += 3;
                         break;
                     case 3, 4:
-                        resultado[0] += 2;
-                        resultado[1] += 1;
+                        // Suma 2 puntos
+                        stats[0] += 2;
                         break;
                     default:
-                        resultado[0] += 1;
-                        resultado[1] += 1;
+                        // Suma 1 punto
+                        stats[0] += 1;
                 }
-                resultado[2] += numIntentos;
+                // Suma los intentos
+                stats[1] += numIntentos;
+                // +1 Partida ganada
+                stats[2] += 1;
                 System.out.println("¡Has ganado!\n");
                 return;
             }
@@ -143,50 +133,90 @@ public class PowerMind {
 
         }
         System.out.println("¡Has perdido! Número máximo de intentos superado.");
-        resultado[3] += 1;
-        resultado[2] += 5;
+        // Suma intentos
+        stats[1] += 5;
+        // +1 Partida perdida
+        stats[3] += 1;
     }
 
-    public static char[] comprobar (int[] secretNum, int[] correctNum){
-        // Array de caracteres para indicar si está, no está o en otra posición
-        char[] sno = new char[4];
-        boolean encontrado = false; //Creamos la bandera
+    // Metodo para generar el numero correcto
+    public static int[] generarNum (int longitud){
+        // Array que vamos a generar
+        int [] numCorrecto = new int[longitud];
+        // Array de booleanos para guardar del 0-9, que se auto rellena con false
+        boolean [] usado = new boolean[10];
+        // Bucle rellena array
+        for (int i = 0; i < numCorrecto.length; i++){
+            // Auxiliar
+            int num;
+            do {
+                // Genera randoms del 0 al 9
+                num = (int) ((Math.random() * 10));
+            } while (usado[num]);
+            numCorrecto[i] = num;
+            // Cambiamos a true la posicion que tiene a num como indice
+            // Así si vuelve a salir, el bucle se ejecutara
+            usado[num] = true;
+        }
+        return numCorrecto;
+    }
 
-        //Hacemos un bucle for para ver que introducimos en el array de caracteres
-        for (int i = 0; i < secretNum.length; i++) {
-            if (secretNum[i] == correctNum[i]) { //Si el número introducido es correcto en el array a adivinar el de caracteres toma 'S'
-                sno[i] = 'S';
-            } else if (secretNum[i] != correctNum[i]) {
-                //Comprobamos si el numero se encuentra en alguna posicion del bucle correcto
-                for (int ii = 0; ii < secretNum.length; ii++) {
-                    encontrado = false;
-                    if (secretNum[i] == correctNum[ii]) { //Si lo encuentra decimos que lo ha encontrado para poner una 'O'
-                        encontrado = true;
+    // Metodo para comprobar el array introducido con el correcto
+    public static char[] comprobar (int[] numIntroducido, int[] numCorrecto){
+        // Array de caracteres para indicar si está, no está o en otra posición
+        char[] sne = new char[4];
+
+        // Bucle for para ver que introducimos en el array de caracteres
+        for (int i = 0; i < numIntroducido.length; i++) {
+            // Condición de posición correcta
+            if (numIntroducido[i] == numCorrecto[i]) {
+                sne[i] = 'S';
+            } else {
+                /* Bucle para ver si existe en otra posicion
+                Dentro del bucle nunca modificamos i, entonces seguimos en la misma posicion en el bucle principal
+                Esto es útil ya que nos permite recorrer el número introducido en cada interación del bucle principal */
+                for (int ii = 0; ii < numIntroducido.length; ii++) {
+                    // Caso default, no encontrado
+                    sne[i] = 'N';
+                    // Condicion de encontrado en otra posicion
+                    if (numIntroducido[i] == numCorrecto[ii]) {
+                        // Sobreescribimos default por O, que indica que existe otra posición
+                        sne[i] = 'E';
                         break;
                     }
                 }
-                //Condicional en el caso de que esté o no esté
-                if (encontrado) {
-                    sno[i] = 'O';
-                } else {
-                    sno[i] = 'N';
-                }
             }
         }
-        return sno;
+        return sne;
     }
 
+    public static void mostrarEstado(int[] numIntroducido, char [] sne) {
+        System.out.println("""
+                       
+                    Recuerda:
+                       S == En la posición correcta
+                       N == No está
+                       O == En posición incorrecta
+                    """);
+
+        System.out.println(Arrays.toString(numIntroducido));
+        System.out.println(" ↑  ↑  ↑  ↑");
+        System.out.println(Arrays.toString(sne));
+    }
+
+    // Metodo que muestra las STATS
     public static void mostrarEstadistica(int[] resultado){
         System.out.printf("""
         ESTADISTICAS
         -------------------------
         Número de puntos obtenidos: %d
-        Numero de partidas ganadas: %d
         Número de intentos: %d
+        Numero de partidas ganadas: %d
         Número de partidas perdidas: %d
         Número de partidas abandonadas: %d
-        """,
-                resultado[0], resultado[1], resultado[2], resultado[3], resultado[4]);
+        -------------------------
+        
+        """, resultado[0], resultado[1], resultado[2], resultado[3], resultado[4]);
     }
 }
 
